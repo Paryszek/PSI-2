4053818af05e4250255cfc56e50de19f196254fb
Paryszek <michal.parysz@yahoo.com>
2018-05-21 16:06:26 +0200

    Basic functionality with trainingData, testData and random w1 w2 b

--- index.js
+++ index.js
@@ -1,28 +1,52 @@
 var _ = require('lodash');
-// http://patrykkobielak.pl/java-sztuczny-neuron/
-// http://patrykkobielak.pl/java-najprostsza-siec-neuronowa/
+
 class Point {
-    constructor(x, y) {
+    constructor(x, y, color) {
         this.x = x;
         this.y = y;
+        this.color = color;
     }
 }
-const numberOfPoints = 10;
-const sizeOfOX = 100;
-const sizeOfOY = 100;
 
-const randomNumber = (value) => Math.floor(Math.random() * value);
-const functionOfActivation = (x, y) 
+const trainData = [
+    new Point(3, 1.5, 'red'),
+    new Point(2, 1, 'blue'),
+    new Point(4, 1.5, 'red'),
+    new Point(3, 1, 'blue'),
+    new Point(3.5, 0.5, 'red'),
+    new Point(2, 0.5, 'blue'),
+    new Point(1, 1, 'red')
+];
 
-let tableOfPoints = [];
-let tableOfValues = [];
+const testData = [
+    new Point(4.5, 1, '')
+];
 
-let w1 = randomNumber(5);
-let w2 = randomNumber(5);
-
-
-for (let i = 0; i < numberOfPoints; i++) {
-    tableOfPoints.push(new Point(randomNumber(sizeOfOX), randomNumber(sizeOfOY)));
+const sigmoid = (value) => {
+    let result = 1 / (1 + Math.pow(Math.E, -value));
+    console.log(result);
+    return result;
 }
-console.log(tableOfPoints);
+
+const randomNumber = () => Math.random();
+
+const training = (point, w1, w2, b) => {
+    return sigmoid(point.x * w1 + point.y + w2 + b);
+}
+
+const check = (value) => {
+    if(value > 0.5) {
+        console.log('blue');
+    } else {
+        console.log('red');
+    }
+}
+
+let w1 = randomNumber();
+let w2 = randomNumber();
+let b = randomNumber();
+
+_.forEach(trainData, (point) => check(training(point, w1, w2, b)));
+
+
 
